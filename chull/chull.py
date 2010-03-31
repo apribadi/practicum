f9655adaac1f218867ff4e8a106513f632b4ded4
Aaron Pribadi <aaron.pribadi@gmail.com>
2010-03-30 20:03:02 -0700

    chull changes

--- chull/chull.py
+++ chull/chull.py
@@ -1,15 +1,17 @@
+import itertools
+
 n = input()
 
 points = []
 count = 0
 for i in range(n):
     x, y, z = raw_input().split()
-    point = map(int, [x, y])
     if z == 'Y':
+        point = (int(x), int(y))
         points.append(point)
         count += 1
 
-first = min(points, key=lambda (x, y): (y, x))
+first = min(points, key=lambda pt: tuple(reversed(pt)))
 ymin = first[1]
 
 ymins = filter(lambda pt: pt[1] == ymin, points)
@@ -21,15 +23,9 @@ def cotan(point):
     dx, dy = x - a, y - b
     return float(dx) / float(dy)
 
+ymins.sort(key=lambda pt: pt[0])
 points.sort(key=lambda pt: -cotan(pt))
 
-def ppt(point):
-    x, y = point
+for pt in itertools.chain(ymins, points):
+    x, y = pt
     print x, y
-
-print count
-for point in sorted(ymins, key=lambda pt: pt[0]):
-    ppt(point)
-for point in points:
-    ppt(point)
-
